c1f41e4ef1feff6174d3afc67414f424559575aa
Marcio Henrique <bnmhcjr@gmail.com>
2026-05-29 22:32:36 -0300

    criação do projeto arrayList

---
+++ array/arrayJava/src/App.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       ArrayList<String> nomes = new ArrayList<>();
+       
+       // adciona elementos a lista add()
+       nomes.add("professor");
+       nomes.add("gilmar");
+       nomes.add("freire");
+       //System.out.println(nomes);
+        //acessar o elemento get(indice)
+        //System.out.println(nomes.get(2));
+        //alterar um item
+        ///nomes.set(0, "prof");
+        //System.out.println(nomes);
+        //remover um item de uma lista
+        //nomes.add("remover este");
+        //System.out.println(nomes);
+        //nomes.remove(3);
+        //System.out.println(nomes);
+
+        //tamanho da lista
+        //System.out.println(nomes.size());
+        
+        //6 apagar a lista
+        //nomes.clear();
+        ///System.out.println(nomes);
+        
+
+        //7 - verificar se existe
+        //System.out.println(nomes.contains("professor"));
+    }
+}
