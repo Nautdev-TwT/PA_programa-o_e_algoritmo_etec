2df101e609b17c11a01a27dadb77c44f9dde61b7
Marcio Henrique <bnmhcjr@gmail.com>
2026-05-29 21:44:35 -0300

    usando scanner

--- vetores/vetores/src/App.java
+++ vetores/vetores/src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner inUser = new Scanner(System.in);
+        System.out.println("digite seu nome");
+        String nome = inUser.nextLine();
+
+        System.out.printf("o nome informado e: %s\n", nome);
+
+
+        inUser.close();
     }
 }
